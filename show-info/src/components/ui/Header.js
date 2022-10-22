26e410d830f77fc4556212b62147a52dd4245053
maanil17 <mlaad17@gmail.com>
2022-10-22 13:38:36 +0530

    made breaking bad info app with breaking bad api

---
+++ show-info/src/components/ui/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import logo from '../../img/logo.png'
+
+const Header = () => {
+  return (
+   
+    <header className='center'>
+        <img src={logo} alt='' />
+
+
+
+
+    </header>
+   
+  )
+}
+
+export default Header
